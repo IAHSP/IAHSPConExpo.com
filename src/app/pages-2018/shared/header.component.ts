5790cb8cbc2e838c8003f7f87eb764d797a2a8d0
Duane Leem <duane@leemfamily.com>
2017-06-15 09:58:57 -0700

    Separated header into its own component.

--- src/app/pages-2018/shared/header.component.ts
+++ src/app/pages-2018/shared/header.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-shared-header',
+    selector: 'app-shared-header-2018',
     template: `
-        `
+        <header id="topNav">
+            <div class="container">
 
-        })
+                <!-- Mobile Menu Button -->
+                <button class="btn btn-mobile" data-toggle="collapse" data-target=".nav-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+
+                <!-- Logo text or image -->
+                <a class="logo scrollTo" href="#wrapper">
+                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/logo.jpg" class="height-100p" alt="IAHSP" />
+                </a>
+
+                <!-- Top Nav -->
+                <div class="navbar-collapse nav-main-collapse collapse pull-right">
+                    <nav class="nav-main mega-menu">
+                        <ul class="nav nav-pills nav-main scroll-menu" id="topMain">
+                            <li class="active"><a class="scrollTo" href="#wrapper">Home</a></li>
+                            <li class="dropdown">
+								<a class="dropdown-toggle" href="#">
+									Information <i class="fa fa-angle-down"></i>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a class="scrollTo" href="#speakershelp">Speakers</a></li>
+                                    <li><a class="scrollTo" href="#vendors">Vendors</a></li>
+								</ul>
+							</li>
+                            <li><a class="padding-top-5 scrollTo" href="#contact"><button type="button" class="btn btn-primary btn-sm">Contact Us</button></a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <!-- /Top Nav -->
+
+            </div>
+        </header>
+    `
+})
 export class HeaderComponent {
 
 }
