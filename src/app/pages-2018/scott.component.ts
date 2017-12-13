a62ad55f1e49fd5e653f9ff08d03cdee38888248
Derrick Leem <derricktleem@gmail.com>
2017-12-13 14:07:52 -0800

    Added Scott Brother's section

---
+++ src/app/pages-2018/scott.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-2018-scott',
+    template: `
+        <section class="padding100">
+            <div class="container ng-scope">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="text-center space-bottom-5"><strong>Jonathan and Drew the Scott Brothers!</strong></h2>
+                        <h3 class="text-center"><em>Hosts of the hit HGTV show "Property Brothers"</em></h3>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/scott-brothers-in-house.jpg" width="320px" class="center-block img-responsive space-bottom-15" alt="Scott Brother's">
+                    </div>
+                    <div class="col-md-8">
+                        <p class="lead">
+                            Entrepreneurs, TV personalities, and best-selling authors, Jonathan and Drew Scott are co-founders of Scott Brothers Global, which includes Scott 
+                            Brothers Entertainment and Scott Living.
+                        </p>
+                        
+                        <p class="lead">
+                            The Scott brothers are hosts of multiple top-rated HGTV series including the Emmy nominated Property Brothers, Brother vs. Brother and Property 
+                            Brothers at Home and their shows are watched by millions of fans in over 150 countries. Jonathan, a renowned designer and licensed contractor, and 
+                            Drew, a seasoned agent and real estate expert, have bought and renovated real estate for over 20 years. The twin brothers have a rare knack for 
+                            finding fixer-uppers and turning them into stunning properties, all while entertaining audiences and followers alike with their winning with more 
+                            than six million followers combined across their social media accounts.
+                        </p>
+                        
+                        <div id="more-scott-info" class="collapse">
+                            <p class="lead">
+                                In addition to their on-screen and social media success, Drew and Jonathan are a rising global lifestyle brand with multiple extensions into music, 
+                                media and retail. They co-founded Scott Brothers Entertainment, which produces award-winning and engaging content in all genres for TV, film and 
+                                digital platforms. The Scott brothers have expanded their brand with the creation of Scott Living, a designer collection of indoor and outdoor 
+                                furniture, décor and bedding available at a variety of North American retailers including QVC, Costco.com and Lowe’s.
+                            </p>
+                                
+                            <p class="lead">
+                                Having grown up on a ranch, spending evenings strumming guitars and singing around campfires, Drew and Jonathan have a true passion for music. In 
+                                2015, they wrote and recorded two original tracks, “Hold On” and “Let the Night Shine In,” with Victoria Shaw and Chad Carlson. Their music video 
+                                for “Hold On” received over 4 million views on both Vevo and YouTube, helping the song hit the Billboard Hot Country charts within the first month 
+                                of its release. In January 2017, the Scott brothers put their own country spin on Flo Rida’s wildly-popular single “My House” with 100% of the 
+                                single’s proceeds being donated to St. Jude Children’s Research Hospital.
+                            </p>
+                                
+                            <p class="lead">
+                                In September 2017, Jonathan and Drew released their memoir, It Takes Two: Our Story. In April 2016, they released Dream Home: The Property 
+                                Brothers’ Ultimate Guide To Finding &amp; Fixing Your Perfect House, a comprehensive guide for any first time homebuyers as well as existing 
+                                homeowners looking to upgrade their space, which quickly became a New York Times and Wall Street Journal best seller.
+                            </p>
+                                
+                            <p class="lead">
+                                Outside of their career endeavors, both Drew and Jonathan serve as global ambassadors for World Vision, working to improve child protection 
+                                domestically and abroad. Their wide-ranging philanthropic contributions include working with a variety of charities such as Artists for Peace 
+                                and Justice, The American Red Cross and St. Jude Children’s Research Hospital, among others. The brothers are also very active when it comes to 
+                                environmental responsibility and the advancement of clean, renewable energies. In December 2016, Jonathan co-hosted 24 Hours of Reality with 
+                                former Vice President Al Gore. It was an incredible success, reaching an audience of over 100 million worldwide.
+                            </p>
+                                
+                            <p class="lead">
+                                Recently, Drew was a contestant on the 25 th season of ABC’s wildly-popular Dancing with the Stars.
+                            </p>
+    
+                            <p class="lead">
+                                The brothers have been featured on The Today Show, The View, Live with Kelly and Ryan, The Rachael Ray Show, Good Morning America, Bloomberg TV 
+                                and Entertainment Tonight, among others, as well as in People, Forbes, Parade, USA Today, Men’s Health, Redbook, The Wall Street Journal, The New 
+                                York Times and more.
+                            </p>
+                        </div>
+
+                        <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#more-scott-info">Read more</button>
+
+                    </div>
+                </div><!-- /row -->
+            </div><!-- /container -->
+        </section>
+    `,
+    styles: [``]
+
+})
+export class Scott2018Component {
+
+}
