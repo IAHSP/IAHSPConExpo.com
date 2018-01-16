73e7168365b8a96bf438a6ecb8237811f8924722
Derrick Leem <derricktleem@gmail.com>
2018-01-16 10:02:02 -0800

    Added LHS video promo

--- src/app/pages-2018/lhsiahsp.component.ts
+++ src/app/pages-2018/lhsiahsp.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
                 <h1 class="text-center"><strong>Luxury Home Staging</strong></h1>
 
                 <div class="row">
+                    <div class="col-md-offset-3 col-md-6 space-bottom-20">
+                        <video width="100%" controls controlsList="nodownload">
+                            <source src="https://d3oaxt0bwkjnjn.cloudfront.net/web/videos/earn-your-iahsp-lhs-luxury-home-staging-designation-hd.mp4" type="video/mp4">
+                        </video>
+                    </div>
                     <div class="col-md-offset-1 col-md-5 space-bottom-40">
                         <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/lhs-logo-blk%20gradient-lux-word.jpg" width="320px" class="center-block img-responsive space-bottom-30" alt="iahsp highpoint wholesale expo">
                     </div>
