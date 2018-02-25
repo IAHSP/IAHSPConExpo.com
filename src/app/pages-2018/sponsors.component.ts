99332bed4e447ce4491fd19f11f420ba4b46f691
Derrick Leem <derricktleem@gmail.com>
2018-02-24 16:28:11 -0800

    Added lots of sponsors

--- src/app/pages-2018/sponsors.component.ts
+++ src/app/pages-2018/sponsors.component.ts
@@ -24,21 +24,26 @@ import { Component } from '@angular/core';
                         classic-home space-bottom-50" alt="Classic Home">
                     </div>
 
+                    <div class="clearfix"></div>
+
+                    <div class="col-md-offset-4 col-md-4">
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/smith-noble.png" width="440px" class="center-block img-responsive pps 
+                        classic-home space-bottom-50" alt="Smith & Noble">
+                    </div>
+
+                    <div class="clearfix"></div>
+
                     <!-- Platinum Sponsors -->
                     <div class="col-md-4 space-8-0 padding-0">
                         <h2 class="text-center"><strong>Platinum Sponsors</strong></h2>
                         <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/dalyn-rug-logo.jpg" class="center-block img-responsive dalyn-rug" width="320px" alt="Dalyn Rug Company Beauty Beyond the Surface">   
                     </div>
+
                     <div class="col-md-8 small-group-imgs">
 
                         <!-- Gold Sponsors -->
                         <div class="col-md-12">  
                             <h2 class="text-center"><strong>Gold Sponsors</strong></h2>
-                            <div class="col-md-6">
-                                <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/elk-group-international.jpg" class="center-block img-responsive" alt="Elk Group International">
-                                </div>
-                            </div>
                             <div class="col-md-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/paragon-logo.jpg" class="center-block img-responsive" alt="Paragon">
@@ -71,6 +76,43 @@ import { Component } from '@angular/core';
                                     img-responsive" alt="Europe 2 You Curate A Life Well Lived">
                                 </div>
                             </div>
+                            <div class="col-md-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/ollix-jla-home.png" class="center-block 
+                                    img-responsive" alt="Ollix/JLA Home">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/props-america.jpg" class="center-block 
+                                    img-responsive" alt="Props America">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/rave-staging-business-training.png" class="center-block 
+                                    img-responsive" alt="Rave Staging Business Training">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uma-enterprises-logo.png" class="center-block 
+                                    img-responsive" alt="UMA Enterprises">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uttermost.png" class="center-block 
+                                    img-responsive" alt="Uttermost">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="small-img-2">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/elk-group-international.jpg" class="center-block img-responsive" alt="Elk Group International">
+                                    <br />
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sterling-furnishing-creativity.jpg" class="center-block img-responsive" alt="Sterling Furnishing Creativity">
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -78,23 +120,31 @@ import { Component } from '@angular/core';
                     <div class="small-group-imgs">
                         <div class="col-md-12">
                             <h2 class="text-center"><strong>Silver Sponsors</strong></h2>
-                            <div class="col-md-offset-4 col-md-4">
+                            <div class="col-md-4">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/churchill.png" class="center-block img-responsive" 
                                     alt="Churchill">
                                 </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <!-- Bronze Sponsors -->
-                    <div class="small-group-imgs">
-                        <div class="col-md-12">
-                            <h2 class="text-center"><strong>Bronze Sponsors</strong></h2>
-                            <div class="col-md-offset-4 col-md-4">
+                            <div class="col-md-4">
                                 <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sherwin-williams.png" class="center-block img-responsive" 
+                                    alt="Sherwin Williams">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/viberent-6-3.png" class="center-block 
+                                    img-responsive" alt="Viberent">
+                                </div>
+                            </div>
+                            <div class="col-md-offset-4 col-md-4">
+                                <div class="small-img-2">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/kmrd-partners-logo.jpg" class="center-block img-responsive" 
-                                    alt="KMRD Partners Inc Risk and Insurance Solutions">
+                                    alt="KMRD Partners">
+                                    <br />
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/trusentry.png" class="center-block img-responsive" 
+                                    alt="Trusentry">
                                 </div>
                             </div>
                         </div>
@@ -159,6 +209,13 @@ import { Component } from '@angular/core';
             display: flex;
             border-radius: 8px;
             padding: 5px 50px;
+            margin: 3px;
+            border: 1px solid lightgray;
+        }
+
+        .small-img-2 {
+            border-radius: 8px;
+            padding: 10px 50px;
             margin: 3px;
             border: 1px solid lightgray;
         }
