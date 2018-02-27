adda750737c66cbf85bc2e70fc5c781f71410a2e
Derrick Leem <derricktleem@gmail.com>
2018-02-27 09:54:34 -0800

    Did minor margin spacing

--- src/app/pages-2018/sponsors.component.ts
+++ src/app/pages-2018/sponsors.component.ts
@@ -12,23 +12,27 @@ import { Component } from '@angular/core';
                         <h2 class="text-center"><strong>Premier Platinum<br />Sponsors</strong></h2>
                     </div>
                     <div class="col-md-4">
-                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/feizy-logo.png" width="440px" class="center-block img-responsive pps feizy 
-                        space-bottom-50" alt="Feizy">
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/classic-home.png" width="440px" class="center-block img-responsive pps 
+                        classic-home space-bottom-25" alt="Classic Home">
                     </div>
                     <div class="col-md-4">
                         <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/coaster-furniture.jpg" width="440px" class="center-block img-responsive pps 
-                        coaster-furniture space-bottom-50" alt="Coaster Furniture">
+                        coaster-furniture space-bottom-25" alt="Coaster Furniture">
                     </div>
                     <div class="col-md-4">
-                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/classic-home.png" width="440px" class="center-block img-responsive pps 
-                        classic-home space-bottom-50" alt="Classic Home">
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/feizy-logo.png" width="440px" class="center-block img-responsive pps feizy 
+                        space-bottom-25" alt="Feizy">
                     </div>
 
                     <div class="clearfix"></div>
 
-                    <div class="col-md-offset-4 col-md-4">
+                    <div class="col-md-offset-2 col-md-4">
                         <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/smith-noble.png" width="440px" class="center-block img-responsive pps 
-                        classic-home space-bottom-50" alt="Smith & Noble">
+                        classic-home space-bottom-25" alt="Smith & Noble">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/surya.png" width="440px" class="center-block img-responsive pps 
+                        surya space-bottom-25" alt="Surya">
                     </div>
 
                     <div class="clearfix"></div>
@@ -36,114 +40,124 @@ import { Component } from '@angular/core';
                     <!-- Platinum Sponsors -->
                     <div class="col-md-4 space-8-0 padding-0">
                         <h2 class="text-center"><strong>Platinum Sponsors</strong></h2>
-                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/dalyn-rug-logo.jpg" class="center-block img-responsive dalyn-rug" width="320px" alt="Dalyn Rug Company Beauty Beyond the Surface">   
+                        <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/dalyn-rug-logo.jpg" class="center-block img-responsive dalyn-rug" width="275px" alt="Dalyn Rug Company Beauty Beyond the Surface">   
                     </div>
 
                     <div class="col-md-8 small-group-imgs">
 
                         <!-- Gold Sponsors -->
-                        <div class="col-md-12">  
+                        <div class="col-md-12 gold">  
                             <h2 class="text-center"><strong>Gold Sponsors</strong></h2>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/paragon-logo.jpg" class="center-block img-responsive" alt="Paragon">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/artists-guild-of-america.jpg" class="center-block 
+                                    img-responsive artists-guild-of-america" alt="Artists Guild of America">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uma-home-decor.jpg" class="center-block img-responsive" alt="UMA Home Decor">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/cort-furniture-rental.jpg" class="center-block img-responsive" alt="Cort 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/cort-furniture-rental.jpg" class="center-block 
+                                    img-responsive cort" alt="Cort 
                                     Furniture Rental">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/d-w-silks.jpg" class="center-block img-responsive" alt="D & W Silks">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/d-w-silks.jpg" class="center-block img-responsive d-w-silks" 
+                                    alt="D & W Silks">
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/artists-guild-of-america.jpg" class="center-block img-responsive" alt="Artists Guild of America">
+                            <div class="col-sm-6">
+                                <div class="small-img-2">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/elk-group-international.jpg" class="center-block img-responsive elk-group" alt="Elk Group International">
+                                    <br />
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sterling-furnishing-creativity.jpg" class="center-block img-responsive sterling" alt="Sterling Furnishing Creativity">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/europe-2-you-curate-a-life-well-lived.png" class="center-block 
                                     img-responsive" alt="Europe 2 You Curate A Life Well Lived">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/ollix-jla-home.png" class="center-block 
                                     img-responsive" alt="Ollix/JLA Home">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/paragon-logo.jpg" class="center-block img-responsive" alt="Paragon">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/props-america.jpg" class="center-block 
-                                    img-responsive" alt="Props America">
+                                    img-responsive props-america" alt="Props America">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/rave-staging-business-training.png" class="center-block 
-                                    img-responsive" alt="Rave Staging Business Training">
+                                    img-responsive rave-staging" alt="Rave Staging Business Training">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uma-enterprises-logo.png" class="center-block 
-                                    img-responsive" alt="UMA Enterprises">
+                                    img-responsive uma-enterprises" alt="UMA Enterprises">
                                 </div>
                             </div>
-                            <div class="col-md-6">
+                            <div class="col-sm-6">
+                                <div class="small-img">
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uma-home-decor.jpg" class="center-block img-responsive" 
+                                    alt="UMA Home Decor">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
                                 <div class="small-img">
                                     <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/uttermost.png" class="center-block 
-                                    img-responsive" alt="Uttermost">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="small-img-2">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/elk-group-international.jpg" class="center-block img-responsive" alt="Elk Group International">
-                                    <br />
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sterling-furnishing-creativity.jpg" class="center-block img-responsive" alt="Sterling Furnishing Creativity">
+                                    img-responsive uttermost" alt="Uttermost">
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     <!-- Silver Sponsors -->
-                    <div class="small-group-imgs">
+                    <div class="clearfix small-group-imgs">
                         <div class="col-md-12">
                             <h2 class="text-center"><strong>Silver Sponsors</strong></h2>
-                            <div class="col-md-4">
+                            <div class="col-sm-4">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/churchill.png" class="center-block img-responsive" 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/churchill.png" width="140px" class="center-block img-responsive" 
                                     alt="Churchill">
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-sm-4">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sherwin-williams.png" class="center-block img-responsive" 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/sherwin-williams-logo.png" width="140px" class="center-block img-responsive" 
                                     alt="Sherwin Williams">
                                 </div>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-sm-4">
                                 <div class="small-img">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/viberent-6-3.png" class="center-block 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/viberent-landscape-logo.png" width="165px" class="center-block 
                                     img-responsive" alt="Viberent">
                                 </div>
                             </div>
+                        </div>
+                    </div>
+
+                    <!-- Bronze Sponsors -->
+                    <div class="small-group-imgs">
+                        <div class="col-md-12">
+                            <h2 class="text-center"><strong>Bronze Sponsors</strong></h2>
                             <div class="col-md-offset-4 col-md-4">
                                 <div class="small-img-2">
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/kmrd-partners-logo.jpg" class="center-block img-responsive" 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/kmrd-partners-logo.jpg" width="160px" class="center-block img-responsive" 
                                     alt="KMRD Partners">
-                                    <br />
-                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/trusentry.png" class="center-block img-responsive" 
+                                    <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/trusentry.png" width="135px" class="center-block img-responsive" 
                                     alt="Trusentry">
                                 </div>
                             </div>
@@ -167,6 +181,10 @@ import { Component } from '@angular/core';
             border-radius: 18px;
         }
 
+        .artists-guild-of-america {
+            height: 95px;
+        }
+
         .coaster-furniture {
             padding: 81px 25px;
         }
@@ -179,6 +197,14 @@ import { Component } from '@angular/core';
             padding: 64px 23px;
         }
 
+        .cort {
+            height: 85px;
+        }
+
+        .d-w-silks {
+            height: 90px;
+        }
+
         .dalyn-rug {
             display: flex;
             border: 1px solid lightgray;
@@ -186,8 +212,33 @@ import { Component } from '@angular/core';
             padding: 25px 20px;
         }
 
-        .dalyn-rug img {
-            height: 200px;
+        .elk-group {
+            max-height: 48px !important;
+            margin-bottom: 5px !important;
+        }
+
+        .props-america {
+            height: 100px;
+        }
+
+        .rave-staging {
+            height: 85px;
+        }
+
+        .sterling {
+            max-height: 50px !important;
+        }
+
+        .surya {
+            padding: 93px 23px;
+        }
+
+        .uma-enterprises {
+            height: 95px;
+        }
+
+        .uttermost {
+            height: 85px;
         }
 
         .small-group-imgs .col-md-6 {
