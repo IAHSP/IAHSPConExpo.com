7540597a5bdc7b01d020a11d5cd94aa8f9bab4bb
Derrick Leem <derricktleem@gmail.com>
2018-03-02 09:47:07 -0800

    Schedule events done

--- src/app/pages-2018/scott.component.ts
+++ src/app/pages-2018/scott.component.ts
@@ -14,7 +14,7 @@ import { Component } from '@angular/core';
                     <div class="col-md-5">
                         <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/20171213-promo-scott-brothers.jpg" class="center-block img-responsive space-bottom-15" alt="Scott Brother's" />
                     </div>
-                    <div class="col-md-7">
+                    <div class="col-md-7 space-bottom-80">
                         <p class="lead">
                             Entrepreneurs, TV personalities, and best-selling authors, Jonathan and Drew Scott are co-founders of Scott Brothers Global, which includes Scott 
                             Brothers Entertainment and Scott Living.
@@ -72,11 +72,240 @@ import { Component } from '@angular/core';
                         <button type="button" class="btn btn-default" data-toggle="collapse" data-target="#more-scott-info">Read more</button>
 
                     </div>
+
+                    <div class="col-md-12">
+
+                        <h2 class="text-center font-weight-400">2018 IAHSP<sup>®</sup> CONFERENCE &amp; EXPO SCHEDULE OF EVENTS</h2>
+
+                        <h3 class="text-center line-height-28">ALL EDUCATIONAL SESSIONS, IAHSP-LHS DESIGNATION AND AWARDS DINNER INCLUDED IN YOUR TICKET PRICE!</h3>
+
+                        <table class="table table-bordered">
+                            <tbody>
+                                <tr class="bg-color-d9d9d9">
+                                    <td class="td-width-150" scope="row"><strong>THURSDAY OCT 18<sup>th</sup></strong></td>
+                                    <td colspan="2"><strong>4:30-10 PM: REGISTRATION AND EXPO 25+ COMPANIES DISPLAYING THEIR PRODUCTS AND SERVICES! 
+                                        INCLUDES FOOD STATIONS, NO-HOST BARS!  PLUS - DJ, KAROKE, DANCING, PHOTO BOOTH</strong> <em>provided by the Charlotte IAHSP® 
+                                        Regional Chapter – the official 2018 HOST CHAPTER</em></td>
+                                </tr>
+
+
+
+                                <tr class="bg-color-d9e2f3">
+                                    <td scope="row"><strong>FRIDAY<br />
+                                        OCT 19<sup>th</sup>:
+                                        </strong></td>
+                                    <td class="text-center"><strong>SPEAKER</strong></td>
+                                    <td class="text-center"><strong>TOPIC</strong></td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">9:00-9:15</td>
+                                    <td><strong>LEADERSHIP TEAM</strong></td>
+                                    <td>WELCOME &amp; OVERVIEW</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">9:15-9:45</td>
+                                    <td><strong>JENNIE NORRIS</strong></td>
+                                    <td>JOURNEY TO THE TOP</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">10:00-10:45</td>
+                                    <td><strong>FRED BERNS</strong></td>
+                                    <td>Selling Methods of the Masters</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">10:50-11:20</td>
+                                    <td><strong>NAHEED OBERFELD</strong></td>
+                                    <td>Mindset Strategies to Boost Your Income</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">11:25-12:15</td>
+                                    <td><strong>JOANNE LENART WEARY</strong></td>
+                                    <td>ONE DAY DECORATING-How to turn this immediate gratification service into income.</td>
+                                </tr>
+
+
+
+                                <!-- BREAKOUT SESSIONS -->
+                                <tr class="bg-color-d9e2f3">
+                                    <td colspan="3"><strong>BREAKOUT SESSIONS - ATTENDEES SELECT 2</strong> – 1 per time slot (sign ups will be done online prior 
+                                        to conference)
+                                        </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:15-1:50</td>
+                                    <td><strong>ANDREA CHENISI</strong></td>
+                                    <td>Create a Winning Team and How to Maintain It</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:15-1:50</td>
+                                    <td><strong>JUNE CARTER</strong></td>
+                                    <td>Timing Sanity, The 34 Minute Time Management Process</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:15-1:50</td>
+                                    <td><strong>CATHERINE LEWIS BROWN</strong></td>
+                                    <td>Pricing Strategies that Work</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:15-1:50</td>
+                                    <td><strong>MARGARET GEHR</strong></td>
+                                    <td>Planning for the Successful Expansion of your Business</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:55-2:30</td>
+                                    <td><strong>ANDREA CHENISI</strong></td>
+                                    <td>Create a Winning Team and How to Maintain it</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:55-2:30</td>
+                                    <td><strong>JUNE CARTER</strong></td>
+                                    <td>Timing Sanity, The 34 Minute Time Management Process</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:55-2:30</td>
+                                    <td><strong>CATHERINE LEWIS BROWN</strong></td>
+                                    <td>Pricing Strategies that Work</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:55-2:30</td>
+                                    <td><strong>MARGARET GEHR</strong></td>
+                                    <td>Planning for the Successful Expansion of your Business</td>
+                                </tr>
+
+
+
+                                <!-- BREAKOUT SESSIONS -->
+                                <tr class="bg-color-d9e2f3">
+                                    <td colspan="3"><strong>BREAKOUT SESSIONS - ATTENDEES 
+                                        SELECT 2</strong> – 1 per time slot (sign ups will 
+                                        be done online prior to conference)</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">2:45-3:20</td>
+                                    <td><strong>KEVIN HTAIN</strong></td>
+                                    <td>Working with the Media</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">2:45-3:20</td>
+                                    <td><strong>CHRISTOPHER CORDER</strong></td>
+                                    <td>Mastering Consultations</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">2:45-3:20</td>
+                                    <td><strong>LAKEITHA SMITH</strong></td>
+                                    <td>LWERKS - Company Management</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">2:45-3:20</td>
+                                    <td><strong>KAT TROULINOS</strong></td>
+                                    <td>Staging for Builders</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">3:25-4:00 PM</td>
+                                    <td><strong>KEVIN HTAIN</strong></td>
+                                    <td>Working with the Media</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">3:25-4:00 PM</td>
+                                    <td><strong>CHRISTOPHER CORDER</strong></td>
+                                    <td>Mastering Consultations</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">3:25-4:00 PM</td>
+                                    <td><strong>LAKEITHA SMITH</strong></td>
+                                    <td>LWERKS - Company Management</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">3:25-4:00 PM</td>
+                                    <td><strong>KAT TROULINOS</strong></td>
+                                    <td>Staging for Builders</td>
+                                </tr>
+
+
+
+                                <!-- KEYNOTE SPEAKERS - THE SCOTT BROTHERS -->
+                                <tr class="bg-color-d9e2f3">
+                                    <td scope="row">4:15-5:15 PM</td>
+                                    <td colspan="2"><span class="text-danger"><strong>KEYNOTE SPEAKERS - THE SCOTT BROTHERS</strong></span></td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">5:00-6:00 PM</td>
+                                    <td colspan="2"><strong>MEET N GREET and BOOK SIGNING</strong></td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">6:30-9:30 PM</td>
+                                    <td colspan="2"><strong>DINNER &amp; AWARDS CEREMONY AND WWSSW<sup>®</sup> FUNDRAISER</strong></td>
+                                </tr>
+
+
+
+                                <!-- SATURDAY OCT 20TH -->
+                                <tr class="bg-color-fff2cc">
+                                    <th scope="row">SATURDAY 
+                                        OCT 20<sup>th</sup>:
+                                        </th>
+                                    <th class="text-center">SPEAKER</th>
+                                    <th class="text-center">TOPIC</th>
+                                </tr>
+                                <tr>
+                                    <td scope="row">9:00 - 9:40 AM</td>
+                                    <th>BARB SCHWARZ - INSPIRATIONAL KEYNOTE SPEAKER</th>
+                                    <td>INSPIRATIONAL TALK</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">9:45AM - 12:45 PM</td>
+                                    <th>LHS DESIGNATION</th>
+                                    <td>LUXURY HOME STAGING DESIGNATION</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">1:45-2:45 PM</td>
+                                    <th>JOSEPH HAECKER</th>
+                                    <td>IAHSP<sup>®</sup> AND PRODUCT INTEGRATION</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">2:50-3:30 PM</td>
+                                    <th>CARRIE LITTLE</th>
+                                    <td>SOCIAL MEDIA STRATEGIES</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">3:45 - 4:15 PM</td>
+                                    <th>MARY JO CAMP</th>
+                                    <td>DESIGN TRENDS</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">4:20-5:00 PM</td>
+                                    <th>SCOTT HANSEN - BUSINESS KEYNOTE SPEAKER</th>
+                                    <td>MARKET DOMINATING POSITIONING, PRODUCTIVITY AND SALES</td>
+                                </tr>
+                                <tr>
+                                    <td scope="row">5:00-5:30 PM</td>
+                                    <th>LEADERSHIP TEAM</th>
+                                    <td>LEADERSHIP TEAM</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div><!-- /row -->
             </div><!-- /container -->
         </section>
     `,
-    styles: [``]
+    styles: [`
+        .td-width-150 {
+            width: 150px;
+        }
+
+        .bg-color-d9d9d9 {
+            background-color: #d9d9d9;
+        }
+
+        .bg-color-d9e2f3 {
+            background-color: #d9e2f3;
+        }
+
+        .bg-color-fff2cc {
+            background-color: #FFF2CC;
+        }
+    `]
 
 })
 export class Scott2018Component {
