04c90c4902912bcd786c2414a5ddc71b79c64087
Duane Leem <Duane@LeemFamily.com>
2018-05-22 10:45:27 -0700

    Updated unit test of main component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,45 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from "./pages-2018/shared/header.component";
+import { Home2018Component } from "./pages-2018/home.component";
+import { Hero2018Component } from "./pages-2018/hero.component";
+import { Scott2018Component } from "./pages-2018/scott.component";
+import { Sponsors2018Component } from "./pages-2018/sponsors.component";
+import { Speakers2018Component } from "./pages-2018/speakers.component";
+import { Vendors2018Component } from "./pages-2018/vendors.component";
+import { Hotel2018Component } from "./pages-2018/hotel.component";
+import { Wholesale2018Component } from "./pages-2018/wholesale.component";
+import { Ast2018Component } from "./pages-2018/ast.component";
+import { Ccs2018Component } from "./pages-2018/ccs.component";
+import { Lhsiahsp2018Component } from "./pages-2018/lhsiahsp.component";
+import { Stageindustawards2018Component } from "./pages-2018/stageindustawards.component";
+import { Countdown2018Component } from "./pages-2018/countdown.component";
+import { Contact2018Component } from "./pages-2018/contact/contact.component";
+import { FooterComponent } from "./pages-2018/shared/footer.component";
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+
+        HeaderComponent,
+        Home2018Component,
+        Hero2018Component,
+        Scott2018Component,
+        Sponsors2018Component,
+        Speakers2018Component,
+        Vendors2018Component,
+        Hotel2018Component,
+        Wholesale2018Component,
+        Ast2018Component,
+        Ccs2018Component,
+        Lhsiahsp2018Component,
+        Stageindustawards2018Component,
+        Countdown2018Component,
+        Contact2018Component,
+        FooterComponent
       ],
     }).compileComponents();
   }));
@@ -15,18 +49,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });
