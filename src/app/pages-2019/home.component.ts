dc972beaf13b7afcb652cb92b30b202339f842bc
Derrick Leem <derricktleem@gmail.com>
2018-11-12 11:45:44 -0800

    Schedule finished

--- src/app/pages-2019/home.component.ts
+++ src/app/pages-2019/home.component.ts
@@ -9,23 +9,29 @@ import { Component, OnInit } from '@angular/core';
 
       <div id="wrapper" class="padding-bottom-0">
 
-        <app-2019-hero></app-2019-hero>
+        <app-2019-hero id="hero"></app-2019-hero>
 
-        <app-2019-event></app-2019-event>
+        <app-2019-event id="event"></app-2019-event>
 
-        <app-2019-speakers></app-2019-speakers>
+        <app-2019-speakers id="speakers"></app-2019-speakers>
 
-        <app-2019-schedule></app-2019-schedule>
+        <app-2019-schedule id="schedule"></app-2019-schedule>
 
-        <app-2019-designation></app-2019-designation>
+        <app-2019-designation id="designation"></app-2019-designation>
 
-        <app-2019-hotel></app-2019-hotel>
+        <app-2019-hotel id="hotel"></app-2019-hotel>
 
       </div>
 
     <app-shared-2019-footer></app-shared-2019-footer>
   `,
-  styles: ['']
+  styles: [`
+      
+      #speakers {
+        text-align: center;
+      }
+
+  `]
 })
 export class Home2019Component implements OnInit {
 
