bf73ec85f0df062b1ddccdce3a5570758b2d8239
Derrick Leem <derricktleem@gmail.com>
2018-12-12 18:42:07 -0800

    Added AST section

--- src/app/pages-2019/designation.component.ts
+++ src/app/pages-2019/designation.component.ts
@@ -10,24 +10,24 @@ import { Component, OnInit } from '@angular/core';
           <div class="col-md-12">
             <p class="lead font-weight-400 text-center">Our IAHSP<sup>®</sup> Conferences are always a GETAWAY with a TAKEAWAY!</p>
 
-            <p class="text-center">This year we are pleased to be issuing:</p>
+            <p class="lead text-center">This year we are pleased to be issuing:</p>
 
             <h2 class="text-center">IAHSP<sup>®</sup> INVESTOR STAGING<br />
             CONSULTANT (ICS) DESIGNATION!</h2>
           </div>
-          <div class="col-md-offset-2 col-md-3">
+          <div class="col-md-offset-1 col-md-4">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/2019/iahsp-investor-staging-consultant-logo.gif" class="center-block 
             img-responsive space-bottom-15" width="320px" alt="IAHSP Investor Staging Consultant Logo">
           </div>
           <div class="col-md-5">
-            <p>Learn from Select Industry Experts on:</p>
-            <ul>
+            <p class="lead">Learn from Select Industry Experts on:</p>
+            <ul class="lead">
               <li>How to Market to Investors</li>
               <li>Pricing Strategies</li>
               <li>Advising on Material Selections/Design</li>
               <li>Staging Investor Properties</li>
             </ul>
-            <p>All attendees will earn the ISC Designation and Logo to demonstrate to the public they have knowledge on how to work with Investors and Stage 
+            <p class="lead">All attendees will earn the ISC Designation and Logo to demonstrate to the public they have knowledge on how to work with Investors and Stage 
             these properties for sale.</p>
           </div>
         </div>
--- src/app/pages-2019/event.component.ts
+++ src/app/pages-2019/event.component.ts
@@ -10,18 +10,17 @@ import { Component, OnInit } from '@angular/core';
           <div class="col-md-12">
             <h2 class="text-center">2019 IAHSP<sup>®</sup> CONFERENCE & EXPO</h2>
           </div>
-          <div class="col-md-offset-1 col-md-5">
+          <div class=" col-md-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/2019/iahsp-conference-and-expo-2019-logo.png" 
             class="center-block img-responsive space-bottom-15" width="420px" alt="IAHSP 2019 Conference & Expo">
-          </div>
-          <div class="col-md-5">            
-            <p>The 2019 IAHSP<sup>®</sup> Conference and EXPO - ROCK Your Business in Music City- brings together top Home Staging Industry business owners and 
+
+            <p class="lead">The 2019 IAHSP<sup>®</sup> Conference and EXPO - ROCK Your Business in Music City- brings together top Home Staging Industry business owners and 
               Real Estate professionals, with key Affiliate Vendors for high-level teaching and networking. The Conference is being held in Nashville, 
               TN with an Advanced Stager Training following the event.</p>
-            <p>Our Conferences are known for key teaching and topics that are pertinent for your success.  We will have Break-out Sessions with topics 
+            <p class="lead">Our Conferences are known for key teaching and topics that are pertinent for your success.  We will have Break-out Sessions with topics 
               geared towards newer Stagers and more experienced Stagers.  Our Speakers are all successful Business owners and our KEY NOTE Speakers from 
               outside the industry bring a different perspective to our event and are always well received.</p>
-            <p>OUR KEYNOTE SPEAKERS! To be Announced!<br />
+            <p class="lead">OUR KEYNOTE SPEAKERS! To be Announced!<br />
               You KNOW we are going to line up great speakers!</p>
           </div>
         </div>
