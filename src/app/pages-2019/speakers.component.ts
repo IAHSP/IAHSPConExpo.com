482208111dcbf87ba00812e83826799efde2fb4f
Derrick Leem <derricktleem@gmail.com>
2019-02-12 10:51:07 -0800

    Setting speakers

--- src/app/pages-2019/speakers.component.ts
+++ src/app/pages-2019/speakers.component.ts
@@ -16,6 +16,65 @@ import { Component, OnInit } from '@angular/core';
           <p class="lead">As we like to share, it is an honor to speak from the platform and we strive to bring quality education and timely topics to our 
             event attendees. If this sounds like YOU - you can send your information to <a href="mailto:jennie@iahsp.com">Jennie@iahsp.com</a>.</p>
         </div>
+
+        <ul class="list-style-none">
+
+          <!-- Speakers -->
+          <li class="col-sm-3">
+            <div class="item-box">
+                <figure>
+                    <img class="img-responsive center-block" src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/fred-berns-320-392.jpg" alt="Fred Berns">
+                </figure>
+                <div class="item-box-desc">
+                    <h4>Fred Berns</h4>
+                    <h5>TOPIC: Selling Methods of the Masters</h5>
+
+                    <!-- modal trigger -->
+                    <div class="text-center">
+                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#four">View Bio</a>
+                    </div>
+
+                    <!-- modal dialog -->
+                    <div class="modal fade" id="four" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                                <!-- modal header -->
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title" id="myModalLabel">Fred Berns</h4>
+                                </div><!-- /modal header -->
+
+                                <!-- modal body -->
+                                <div class="modal-body">
+                                    <p>Fred Berns is among the biggest names in the <em>business</em> of interior design.  He’s an award-winning coach, promotional 
+                                    copywriter and professional speaker who trains and creates promotional materials for design professionals and real estate 
+                                    stagers worldwide, helping them promote themselves more effectively and dramatically increase sales and profits.</p>
+                                    
+                                    <p>His training and coaching sessions for interior designers, stagers, window fashion professionals, kitchen and bath 
+                                    specialists, and other design professionals from Dallas to Dubai focus on how they can double their sales, connect with 
+                                    better clients, and make a maximum marketing impact for a minimal investment of time and money.</p>
+
+                                    <p>Fred presents sales and marketing programs at design centers, marts and conferences worldwide. He was an emcee and 
+                                    presenter at the International Federation of Interior Architects and Designers(IFI) in Dubai, U.A.E.  In addition, 
+                                    he has been a featured speaker at the International Window Coverings Expo; Neocon events; the High Point Market; 
+                                    Surfaces; design symposiums; home and design expos; furniture, gift and accessory shows; and national conferences of 
+                                    such groups as ASID, NKBA, IFDA and Decorating Den.  Fred’s blog,  <a href="https://interiordesignbusiness.net/">
+                                    InteriorDesignBusiness.net</a>, reaches thousands of design professionals around the world. He also regularly writes 
+                                    for a variety of design trade publications and online outlets.</p>
+                                </div><!-- /modal body -->
+
+                                <!-- modal footer -->
+                                <div class="modal-footer">
+                                    <button class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div><!-- /modal footer -->
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </li>
       </div>
     </div>
   </section>
