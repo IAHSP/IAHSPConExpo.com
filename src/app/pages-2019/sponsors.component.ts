4943f3b86799d2eb83d3521367991b7d3ab625d8
Derrick Leem <derricktleem@gmail.com>
2019-03-21 10:54:32 -0700

    Diamond, Platinum, and Gold sponsors finished

--- src/app/pages-2019/sponsors.component.ts
+++ src/app/pages-2019/sponsors.component.ts
@@ -7,13 +7,14 @@ import { Component, OnInit } from '@angular/core';
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <h2><strong>Sponsors</strong></h2>
+            <h2 class="text-center"><strong>Sponsors</strong></h2>
 
             <!-- Diamond -->
             <h3 class="text-center">Diamond</h3>
           </div>
-          <div class="col-sm-offset-2 col-sm-8">
-            <img src="" alt="">
+          <div class="col-sm-offset-1 col-sm-10">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/coaster-logo.png" 
+            class="center-block img-responsive" alt="Coaster Sponsor Logo">
           </div>
 
           <!-- Platinum -->
@@ -21,22 +22,28 @@ import { Component, OnInit } from '@angular/core';
             <h3 class="text-center">Platinum</h3>
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/classic-home-logo.png" 
+            class="center-block img-responsive" alt="Classic Home Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/essentials-for-living-with-two-companies-logo.jpg" 
+            class="center-block img-responsive" alt="Essentials For Living Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/company-c-logo.png" 
+            class="center-block img-responsive" alt="Company C Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/dalyn-logo.png" 
+            class="center-block img-responsive" alt="Dalyn Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/universal-furniture-logo.png" 
+            class="center-block img-responsive" alt="Universal Furniture Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/zoom-casa-logo.png" 
+            class="center-block img-responsive" alt="Zoom Casa Sponsor Logo">
           </div>
 
           <!-- Gold -->
@@ -44,28 +51,36 @@ import { Component, OnInit } from '@angular/core';
             <h3 class="text-center">Gold</h3>
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/aflac-logo.png" 
+            class="center-block img-responsive" alt="Aflac Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/cubiqz-logo.png" 
+            class="center-block img-responsive" alt="Cubiqz Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/ollix-logo.png" 
+            class="center-block img-responsive" alt="Olliix Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/paragon-logo.png" 
+            class="center-block img-responsive" alt="Paragon Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/propac-logo.png" 
+            class="center-block img-responsive" alt="Propac Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/rave-staging-redesign-logo.png" 
+            class="center-block img-responsive" alt="Rave Staging Redesign Sponsor Logo">
           </div>
           <div class="col-sm-offset-2 col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/srm-staging-policy-logo.jpg" 
+            class="center-block img-responsive" alt="SRM Staging Policy Sponsor Logo">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/banners/shc-table-banner.jpg" 
+            class="center-block img-responsive" alt="Staged Homes Banner">
           </div>
 
           <!-- Silver -->
@@ -73,13 +88,16 @@ import { Component, OnInit } from '@angular/core';
             <h3 class="text-center">Silver</h3>
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="" 
+            class="center-block img-responsive" alt="">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="" 
+            class="center-block img-responsive" alt="">
           </div>
           <div class="col-sm-4">
-            <img src="" alt="">
+            <img src="" 
+            class="center-block img-responsive" alt="">
           </div>
         </div>
       </div>
