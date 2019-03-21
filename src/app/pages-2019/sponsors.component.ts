d9e782e49ea6acf9e043f507fd594eb53eb99f29
Derrick Leem <derricktleem@gmail.com>
2019-03-21 10:18:44 -0700

    Laid out HTML

--- src/app/pages-2019/sponsors.component.ts
+++ src/app/pages-2019/sponsors.component.ts
@@ -1,11 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sponsors',
+  selector: 'app-2019-sponsors',
   template: `
-    <p>
-      sponsors works!
-    </p>
+    <section class="padding100">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2><strong>Sponsors</strong></h2>
+
+            <!-- Diamond -->
+            <h3 class="text-center">Diamond</h3>
+          </div>
+          <div class="col-sm-offset-2 col-sm-8">
+            <img src="" alt="">
+          </div>
+
+          <!-- Platinum -->
+          <div class="col-sm-12">
+            <h3 class="text-center">Platinum</h3>
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+
+          <!-- Gold -->
+          <div class="col-sm-12">
+            <h3 class="text-center">Gold</h3>
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-offset-2 col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+
+          <!-- Silver -->
+          <div class="col-sm-12">
+            <h3 class="text-center">Silver</h3>
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+          <div class="col-sm-4">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
   `,
   styles: []
 })
