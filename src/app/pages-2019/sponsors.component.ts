7574e29135820aa6ed13e271c45ea13653122342
Derrick Leem <derricktleem@gmail.com>
2019-03-21 09:28:11 -0700

    Added sponsors for 2019

---
+++ src/app/pages-2019/sponsors.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsors',
+  template: `
+    <p>
+      sponsors works!
+    </p>
+  `,
+  styles: []
+})
+export class SponsorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
