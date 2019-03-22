6ac96f664915131444fb9d71decfae3b6843c44a
Derrick Leem <derricktleem@gmail.com>
2019-03-22 12:04:45 -0700

    Minor updates

--- src/app/pages-2019/sponsors.component.ts
+++ src/app/pages-2019/sponsors.component.ts
@@ -3,107 +3,207 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-2019-sponsors',
   template: `
-    <section class="padding100">
+    <section class="padding100 bg-white">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <h2 class="text-center"><strong>Sponsors</strong></h2>
-
-            <!-- Diamond -->
-            <h3 class="text-center">Diamond</h3>
+            <h2 class="text-center"><strong>SPONSORS</strong></h2>            
           </div>
-          <div class="col-sm-offset-1 col-sm-10">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/coaster-logo.png" 
-            class="center-block img-responsive" alt="Coaster Sponsor Logo">
+
+          <!-- Diamond -->
+          <div class="diamond">
+            <div class="col-sm-offset-1 col-sm-10">
+              <h3 class="text-center color-diamond"><strong>Diamond</strong></h3>
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/coaster-logo.png" 
+              class="center-block img-responsive space-bottom-80" alt="Coaster Sponsor Logo">
+            </div>
           </div>
 
           <!-- Platinum -->
-          <div class="col-sm-12">
-            <h3 class="text-center">Platinum</h3>
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/classic-home-logo.png" 
-            class="center-block img-responsive" alt="Classic Home Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/essentials-for-living-with-two-companies-logo.jpg" 
-            class="center-block img-responsive" alt="Essentials For Living Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/company-c-logo.png" 
-            class="center-block img-responsive" alt="Company C Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/dalyn-logo.png" 
-            class="center-block img-responsive" alt="Dalyn Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/universal-furniture-logo.png" 
-            class="center-block img-responsive" alt="Universal Furniture Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/zoom-casa-logo.png" 
-            class="center-block img-responsive" alt="Zoom Casa Sponsor Logo">
+          <div class="platinum">
+            <div class="col-sm-12">
+              <h3 class="text-center color-platinum"><strong>Platinum</strong></h3>
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/classic-home-logo.png" 
+              class="center-block img-responsive classic" alt="Classic Home Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/essentials-for-living-with-two-companies-logo.jpg" 
+              class="center-block img-responsive essentials" alt="Essentials For Living Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/company-c-logo.png" 
+              class="center-block img-responsive" alt="Company C Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+
+            <div class="clearfix space-bottom-30"></div>
+
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/dalyn-logo.png" 
+              class="center-block img-responsive" alt="Dalyn Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/universal-furniture-logo.png" 
+              class="center-block img-responsive universal" alt="Universal Furniture Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/zoom-casa-logo.png" 
+              class="center-block img-responsive zoom space-bottom-80" alt="Zoom Casa Sponsor Logo">
+            </div>
           </div>
 
           <!-- Gold -->
-          <div class="col-sm-12">
-            <h3 class="text-center">Gold</h3>
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/aflac-logo.png" 
-            class="center-block img-responsive" alt="Aflac Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/cubiqz-logo.png" 
-            class="center-block img-responsive" alt="Cubiqz Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/ollix-logo.png" 
-            class="center-block img-responsive" alt="Olliix Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/paragon-logo.png" 
-            class="center-block img-responsive" alt="Paragon Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/propac-logo.png" 
-            class="center-block img-responsive" alt="Propac Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/rave-staging-redesign-logo.png" 
-            class="center-block img-responsive" alt="Rave Staging Redesign Sponsor Logo">
-          </div>
-          <div class="col-sm-offset-2 col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/srm-staging-policy-logo.jpg" 
-            class="center-block img-responsive" alt="SRM Staging Policy Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/banners/shc-table-banner.jpg" 
-            class="center-block img-responsive" alt="Staged Homes Banner">
+          <div class="gold">
+            <div class="col-sm-12">
+              <h3 class="text-center color-gold"><strong>Gold</strong></h3>
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/aflac-logo.png" 
+              class="center-block img-responsive aflac" alt="Aflac Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/cubiqz-logo.png" 
+              class="center-block img-responsive" alt="Cubiqz Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/ollix-logo.png" 
+              class="center-block img-responsive olliix" alt="Olliix Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+
+            <div class="clearfix space-bottom-30"></div>
+
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/paragon-logo.png" 
+              class="center-block img-responsive paragon" alt="Paragon Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/propac-logo.png" 
+              class="center-block img-responsive propac" alt="Propac Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/rave-staging-redesign-logo.png" 
+              class="center-block img-responsive" alt="Rave Staging Redesign Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+
+            <div class="clearfix space-bottom-30"></div>
+
+            <div class="col-sm-offset-2 col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/srm-staging-policy-logo.jpg" 
+              class="center-block img-responsive srm" alt="SRM Staging Policy Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/banners/shc-table-banner.jpg" 
+              class="center-block img-responsive space-bottom-80" alt="Staged Homes Banner">
+            </div>
           </div>
 
           <!-- Silver -->
+          <div class="silver">
+            <div class="col-sm-12">
+              <h3 class="text-center color-silver"><strong>Silver</strong></h3>
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/elk-group-logo.png" 
+              class="center-block img-responsive elk" alt="ELK Group Sponsor Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/jlw-company-logo.jpg" 
+              class="center-block img-responsive" alt="JLW Company Logo">
+              <hr class="visible-xs" />
+            </div>
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/the-successful-stager-logo.png" 
+              class="center-block img-responsive successful space-bottom-80" alt="The Successful Stager Sponsor Logo">
+            </div>
+          </div>
+
           <div class="col-sm-12">
-            <h3 class="text-center">Silver</h3>
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/elk-group-logo.png" 
-            class="center-block img-responsive" alt="ELK Group Sponsor Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/jlw-company-logo.jpg" 
-            class="center-block img-responsive" alt="JLW Company Logo">
-          </div>
-          <div class="col-sm-4">
-            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/the-successful-stager-logo.png" 
-            class="center-block img-responsive" alt="The Successful Stager Sponsor Logo">
+            <h2 class="text-center"><em><strong>and MORE being added every week!</strong></em></h2>
           </div>
         </div>
       </div>
     </section>
   `,
-  styles: []
+  styles: [`
+  
+    img { margin-bottom: 15px; }
+
+    /* Platinum */
+    .platinum img {
+      width: 90%;
+    }
+
+    /* Gold */
+    .gold img {
+      width: 78%;
+    }
+
+    /* Silver */
+    .silver img {
+      width: 58%;
+    }
+
+    @media screen and (min-width: 768px) {
+
+      /* Platinum */
+      .classic {
+        padding: 14px 0;
+      }
+
+      .essentials {
+        padding: 3px 0;
+      }
+
+      .universal {
+        padding: 69px 0;
+      }
+
+      .zoom {
+        padding: 57px 0;
+      }
+
+      /* Gold */
+      .aflac {
+        padding: 21px 0;
+      }
+
+      .olliix {
+        padding: 22px 0;
+      }
+
+      .paragon {
+        padding: 34px 0;
+      }
+
+      .propac {
+        padding: 10px 0;
+      }
+
+      .srm {
+        padding: 62px 0;
+      }
+
+      /* Silver */
+      .elk, .successful {
+        padding: 33px 0;
+      }
+    }
+    
+  `]
 })
 export class SponsorsComponent implements OnInit {
 
