508ff10d29dfa83f0a07ab3515c1be32b0b0fdf9
Derrick Leem <derricktleem@gmail.com>
2019-06-26 17:44:53 -0700

    Added 3 speakers at AST

--- src/app/pages-2019/ast.component.ts
+++ src/app/pages-2019/ast.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { astList } from './shared/speakers';
+
+
 
 @Component({
   selector: 'app-2019-ast',
@@ -20,6 +23,37 @@ import { Component, OnInit } from '@angular/core';
             <img alt="Advanced Stager Training 2019 Promo" src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/ast-2019-promo-flyer.jpg" 
             class="space-bottom-40 center-block img-responsive" width="600px">
 
+            <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/advance-stager-training-logo.jpg" class="center-block img-responsive space-bottom-15" 
+            alt="Advance Stager Training Logo">
+
+            <div class="row">
+              <div class="col-md-12 space-bottom-40">
+                <ul class="no-bullets padding-left-0">
+                  <ng-container *ngFor="let ast of asts; let i = index;">
+                    <div *ngIf="(ast % 3) == 0" class="clearfix visible-md"></div>
+      
+                    <!-- asts -->
+                    <li class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+                      <div class="item-box">
+                        <figure>
+                          <img class="img-responsive center-block" src="{{ ast.image }}" alt="{{ ast.name }}">
+                        </figure>
+                        <div class="item-box-desc">
+                          <h4 class="space-bottom-8 text-center">{{ ast.name }}<br />
+                          <small class="text-center">{{ ast.company }}</small></h4>
+                          <h5 class="text-center">{{ ast.title }}</h5>                         
+                        </div>
+                      </div>
+                    </li>
+                    <!-- /asts -->
+
+                    <div class="clearfix visible-sm visible-md" *ngIf="( i + 1 ) % 3 == 0"></div>
+                    <div class="clearfix visible-lg" *ngIf="( i + 1 ) % 4 == 0"></div>
+                  </ng-container>
+                </ul>
+              </div>
+            </div>
+
             <p class="lead text-center"><strong>This year, we are excited to have six workshop facilitators who are very qualified to be part of this event:</strong></p>
 
             <p class="lead text-center">Advanced Stager Training Day<br />
@@ -30,7 +64,7 @@ import { Component, OnInit } from '@angular/core';
             
             <div class="text-center">
               <a href="https://pages.iahsp.com/advanced-stager-training/" class="btn btn-primary color-white">Register Now</a>
-            </div>
+            </div>            
           </div>
         </div>
       </div>
@@ -43,9 +77,20 @@ import { Component, OnInit } from '@angular/core';
       color: #2D2926;
     }
 
+    h4 > small {
+      line-height: 1.2;
+      margin-top: 2px;
+    }
+
+    .no-bullets {
+      list-style: none;
+    }
+
   `]
 })
 export class Ast2019Component implements OnInit {
+
+  asts = astList;
 
   constructor() { }
 
