dbc8a08b89a1be4e5fac373f43fffeeec4daf6c8
Derrick Leem <derricktleem@gmail.com>
2019-08-08 10:45:49 -0700

    New logos

--- src/app/pages-2019/sponsors.component.ts
+++ src/app/pages-2019/sponsors.component.ts
@@ -13,15 +13,18 @@ import { Component, OnInit } from '@angular/core';
           <!-- Diamond -->
           <div class="col-sm-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/diamond-sponsor.jpg" 
-            class="center-block img-responsive space-bottom-20" alt="Diamond Sponsors" width="240px">
+            class="center-block space-bottom-20" alt="Diamond Sponsors" width="240px">
           </div>
           <div class="diamond">
             <div class="col-sm-6">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/coaster-logo.png" 
               class="center-block img-responsive coaster" alt="Coaster Sponsor Logo">
-            </div>
-            <hr class="visible-xs" />
+            </div><hr class="visible-xs" />
             <div class="col-sm-6">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/karastan-logo.png" 
+              class="center-block img-responsive karastan" alt="Karastan Sponsor Logo">
+            </div><hr class="visible-xs" />
+            <div class="col-sm-offset-3 col-sm-6">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/spectra-home-logo.png" 
               class="center-block img-responsive space-bottom-80" alt="Coaster Sponsor Logo" width="323px">
             </div>
@@ -30,37 +33,32 @@ import { Component, OnInit } from '@angular/core';
           <!-- Platinum -->
           <div class="col-sm-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/platinum-sponsor.gif" 
-            class="center-block img-responsive space-bottom-20" alt="Platinum Sponsors" width="240px">
+            class="center-block space-bottom-20" alt="Platinum Sponsors" width="240px">
           </div>
           <div class="platinum">            
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/bedford-logo.png" 
-              class="center-block img-responsive bedford" alt="Bedford Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive bedford" alt="Bedford Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/classic-home-logo.png" 
-              class="center-block img-responsive classic" alt="Classic Home Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive classic" alt="Classic Home Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/essentials-for-living-with-two-companies-logo.jpg" 
-              class="center-block img-responsive essentials" alt="Essentials For Living Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>            
+              class="center-block img-responsive essentials" alt="Essentials For Living Sponsor Logo">              
+            </div><hr class="visible-xs" />            
 
-            <div class="clearfix space-bottom-30"></div>
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
 
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/dalyn-logo.png"
-              class="center-block img-responsive" alt="Dalyn Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive" alt="Dalyn Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/universal-furniture-logo.png" 
-              class="center-block img-responsive universal" alt="Universal Furniture Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive universal" alt="Universal Furniture Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/zoom-casa-logo.png" 
               class="center-block img-responsive zoom space-bottom-80" alt="Zoom Casa Sponsor Logo">
@@ -70,127 +68,134 @@ import { Component, OnInit } from '@angular/core';
           <!-- Gold -->
           <div class="col-sm-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/gold-sponsor.gif" 
-            class="center-block img-responsive space-bottom-20" alt="Gold Sponsors" width="240px">
+            class="center-block space-bottom-20" alt="Gold Sponsors" width="240px">
           </div>
           <div class="gold">
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/aflac-logo.png" 
-              class="center-block img-responsive aflac" alt="Aflac Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block aflac" alt="Aflac Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/artist-guild-of-america.jpg" 
-              class="center-block img-responsive" alt="Artist Guild of America Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive artist-guild" alt="Artist Guild of America Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/black-swan-boutique.png" 
-              class="center-block img-responsive" alt="Black Swan Boutique Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block img-responsive" alt="Black Swan Boutique Sponsor Logo">              
+            </div><hr class="visible-xs" />
             
-            <div class="clearfix space-bottom-30"></div>            
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>         
             
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/couture-cashmere-logo.png" 
+              class="center-block couture-cashmere" alt="Couture Cashmere Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/cubiqz-logo.png" 
-              class="center-block img-responsive cubiqz" alt="Cubiqz Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block cubiqz" alt="Cubiqz Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/diamond-sofa-logo.png" 
-              class="center-block img-responsive" alt="Diamond Sofa Logo">
-              <hr class="visible-xs" />
-            </div>            
+              class="center-block img-responsive" alt="Diamond Sofa Logo">              
+            </div><hr class="visible-xs" />
+            
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
+            
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/jutus-logo.png" 
-              class="center-block img-responsive jutus" alt="Jutus Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
-            
-            <div class="clearfix space-bottom-30"></div>
-            
+              class="center-block jutus" alt="Jutus Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/ollix-logo.png" 
-              class="center-block img-responsive olliix" alt="Olliix Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block olliix" alt="Olliix Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/paragon-logo.png" 
-              class="center-block img-responsive paragon" alt="Paragon Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block paragon" alt="Paragon Sponsor Logo">              
+            </div><hr class="visible-xs" />
+            
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
+            
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/propac-logo.png" 
-              class="center-block img-responsive propac" alt="Propac Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
-            
-            <div class="clearfix space-bottom-30"></div>
-            
+              class="center-block propac" alt="Propac Sponsor Logo">              
+            </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/rave-staging-redesign-logo.png" 
-              class="center-block img-responsive rave" alt="Rave Staging Redesign Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
+              class="center-block rave" alt="Rave Staging Redesign Sponsor Logo">
+            </div><hr class="visible-xs" />
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/sagebrook-home-logo.jpg" 
+              class="center-block img-responsive" alt="Sagebrook Sponsor Logo">              
+            </div><hr class="visible-xs" />
+
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
+
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/sherwin-williams-logo.png" 
-              class="center-block img-responsive sherwin-williams" alt="Sherwin Williams Sponsor Logo">
-              <hr class="visible-xs" />
-            </div>
-            <div class="col-sm-4">
-              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/srm-staging-policy-logo.jpg" 
-              class="center-block img-responsive srm space-bottom-80" alt="SRM Staging Policy Sponsor Logo">
-            </div>            
+              class="center-block sherwin-williams space-bottom-80" alt="Sherwin Williams Sponsor Logo">
+            </div>    
           </div>
 
           <!-- Silver -->
           <div class="col-sm-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/silver-sponsor.gif" 
-            class="center-block img-responsive space-bottom-20" alt="Silver Sponsors" width="240px">
+            class="center-block space-bottom-20" alt="Silver Sponsors" width="240px">
           </div>
           <div class="silver">
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/barb-schwarz-logo.png" 
-              class="center-block img-responsive barb" alt="Barb Schwarz Sponsor Logo">              
+              class="center-block barb" alt="Barb Schwarz Sponsor Logo">              
             </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/elk-group-logo.png" 
-              class="center-block img-responsive elk" alt="ELK Group Sponsor Logo">              
+              class="center-block elk" alt="ELK Group Sponsor Logo">              
             </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/home-staging-and-redesign-association-logo.png" 
-              class="center-block img-responsive hsra" alt="Home Staging and Redesign Association Logo">
+              class="center-block hsra" alt="Home Staging and Redesign Association Logo">
             </div><hr class="visible-xs" />            
 
-            <div class="clearfix"></div>
-
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
+            
             <div class="col-sm-4">
-              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/jlw-company-logo.jpg" 
-              class="center-block img-responsive" alt="JLW Company Logo">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/kmrd-partners-logo.jpg" 
+              class="center-block kmrd" alt="KMRD Partners Company Logo">
             </div><hr class="visible-xs" />
             <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/banners/shc-table-banner.jpg" 
-              class="center-block img-responsive space-bottom-80" alt="Staged Homes Banner">
+              class="center-block" alt="Staged Homes Banner">
             </div><hr class="visible-xs" />
             <div class="col-sm-4">
-              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/the-successful-stager-logo.png" 
-              class="center-block img-responsive successful space-bottom-80" alt="The Successful Stager Sponsor Logo">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/stageforce-logo.png" 
+              class="center-block stageforce space-bottom-80" alt="Stageforce Sponsor Logo">
             </div>
           </div>
 
           <!-- Bronze -->
           <div class="col-sm-12">
             <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/bronze-logo.png" 
-            class="center-block img-responsive space-bottom-20" alt="Bronze Sponsors" width="240px">
+            class="center-block space-bottom-20" alt="Bronze Sponsors" width="240px">
           </div>
           <div class="bronze">
-            <div class="col-sm-offset-2 col-sm-4">
+            <div class="col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/decorating-and-staging-academy.jpg" 
-              class="center-block img-responsive space-bottom-80 dsa" alt="Decorating and Staging Academy Logo" width="135px">
+              class="center-block dsa" alt="Decorating and Staging Academy Logo" width="135px">
             </div><hr class="visible-xs" />
             <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/jlw-company-logo.jpg" 
+              class="center-block jlw" alt="JLW Company Logo">
+            </div><hr class="visible-xs" />
+            <div class="col-sm-4">
+              <img src="https://d3oaxt0bwkjnjn.cloudfront.net/web/images/logos/the-successful-stager-logo.png" 
+              class="center-block" alt="The Successful Stager Sponsor Logo">
+            </div>
+
+            <div class="col-sm-12 hidden-xs clearfix space-bottom-30"></div>
+
+            <div class="col-sm-offset-4 col-sm-4">
               <img src="https://d3oaxt0bwkjnjn.cloudfront.net/images/simply-splendid-logo.png" 
-              class="center-block img-responsive space-bottom-80 simply-splendid" alt="Simply Splendid Sponsor Logo" width="135px">
+              class="center-block space-bottom-80 simply-splendid" alt="Simply Splendid Sponsor Logo" width="135px">
             </div>
           </div>
 
@@ -225,6 +230,9 @@ import { Component, OnInit } from '@angular/core';
       .coaster {
         padding: 78px 0;
       }
+      .karastan {
+        padding: 33px 0;
+      }
 
       /* Platinum */
       .bedford {
@@ -245,10 +253,16 @@ import { Component, OnInit } from '@angular/core';
 
       /* Gold */
       .aflac {
-        padding: 45px 0;
+        padding: 71px 0;
+      }
+      .artist-guild {
+        padding: 25px 0;
+      }
+      .couture-cashmere {
+        padding: 43px 0;
       }
       .cubiqz {
-        padding: 31px 0;
+        padding: 3px 0;
       }
       .jutus {
         padding: 3px 0;
@@ -260,13 +274,13 @@ import { Component, OnInit } from '@angular/core';
         padding: 34px 0;
       }
       .propac {
-        padding: 10px 0;
+        padding: 60px 0;
       }
       .rave {
-        padding: 64px 0;
+        padding: 55px 0;
       }
       .sherwin-williams {
-        padding: 79px 0;
+        /* padding: 79px 0; */
       }
       .srm {
         padding: 62px 0;
@@ -279,9 +293,17 @@ import { Component, OnInit } from '@angular/core';
       .elk, .hsra {
         padding: 76px 0;
       }
-      .successful {
-        padding: 27px 0;
+      .kmrd {
+        padding: 26px 0;
       }
+      .stageforce {
+        padding: 30px 0;
+      }
+      
+      /* Bronze */
+      .jlw {
+        padding: 7px 0;
+      }      
     }
     
   `]
