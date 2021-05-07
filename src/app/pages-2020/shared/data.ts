c324c374ea28e2040244d2dfbe97f47b57ed5fbb
Derrick Leem <derricktleem@gmail.com>
2021-05-07 15:49:16 -0700

    Updated Ravi's Verbiage

--- src/app/pages-2020/shared/data.ts
+++ src/app/pages-2020/shared/data.ts
@@ -118,26 +118,7 @@ export const cdeList = [
   },
 ];
 
-export const ksList = [
-  {
-    image: `https://d3oaxt0bwkjnjn.cloudfront.net/images/chris-widener.jpg`,
-    name: `Chris Widener`,
-    company: `Motivational Speaker and Business Success Expert`,
-    title: `Best selling author of 21 books, world renown business expert top 50 motivational speakers in the WORLD`,
-    modelId: `tA2ey`,
-    description: `
-		
-			<p><strong>Chris Widener</strong> is a best-selling author, motivational speaker, and business success expert who has worked closely with Jim Rohn and Zig Ziglar. Chris 
-			hosted a popular television show with Zig, and co-authored books with Jim Rohn, a highly respected and recognized motivational speaker and educator.</p>
-			
-			<p>Chris is the author of 20 books which have been translated into 13 languages, including his book, The Art of Influence, which his popular keynote is based 
-			upon. He has been speaking professionally since 1988 and has given over 2,000 speeches to crowds as large as 25,000 people!</p>
-			
-			<p>He has also been named one of the <strong>Top 50 Speakers in the World, one of Inc. Magazine's Top 100 Leadership speakers, and is a member of the Motivational 
-			Speakers Hall of Fame.</strong> Chris’ 21st book will be released this Fall, <em>“Lasting Impact, Creating a Life and a Business that Lives Beyond You”.</em></p>
-		
-		`,
-  },
+export const ksList = [  
   {
     image: `https://d3oaxt0bwkjnjn.cloudfront.net/images/barber-deshauna.jpg`,
     name: `Deshauna Barber`,
@@ -168,7 +149,7 @@ export const ksList = [
 
 		`,
   },
-  {
+	{
     image: `https://d3oaxt0bwkjnjn.cloudfront.net/images/jonny-fowler.jpg`,
     name: `Jonny Fowler`,
     company: `Speaker | Podcaster | Leadership Coach | Senior VP`,
@@ -226,19 +207,56 @@ export const ksList = [
 		
 		`,
   },
-  {
+	{
     image: `https://d3oaxt0bwkjnjn.cloudfront.net/images/ravi-hutheesing.jpg`,
     name: `Ravi Hutheesing`,
     company: `International Keynote Speaker`,
     title: `PIVOT, the Difference Between Staying Relevant and Becoming Redundant`,
     modelId: `gUgAg`,
-    description: `
+    description: `		
 		
-			<p><strong>PIVOT, the Difference Between Staying Relevant and Becoming Redundant</strong>. It teaches the art of reinventing yourself, your 
-			business, etc.; how to recognize and create opportunities that convert; building cross-generational and cross-cultural networks, 
-			life and career planning along with ongoing “course correction,” developing a tolerance for risk and a willingness to fail (and 
-				recover), and much more.</p>
+			<p><strong>A Journey of Pivots Unlike Any Other</strong></p>
+			
+			<p>Ravi is an international keynote speaker who empowers education and business leaders to pivot for success in multi-cultural and multigenerational 
+			environments. Born into a lineage of prime ministers and raised by a family of Wall Street bankers, his journey as a rock star, aviator, and cultural 
+			diplomat for the US Dept. of State is an inspiring example of how to stay relevant in an ever-changing world.</p>
+			
+			<p>The first American-born member of the family that created and governed the world's largest democracy for over 40 years, Ravi is the grand-nephew of 
+			Jawaharlal Nehru (India's first prime minister) and the cousin of prime ministers Indira Gandhi and Rajiv Gandhi.</p>
+			
+			<p>He became an artist-entrepreneur before graduating high school, but Ravi's worldwide visibility skyrocketed in 1997 as the guitarist for triple Grammy 
+			nominee, Hanson. Their massive millennial fanbase catapulted them to the White House, Madison Square Garden, Saturday Night Live, and more. He continued 
+			performing and began giving artist-entrepreneurship keynotes sponsored by the music products industry.</p>
+			
+			<p>In 2008, Ravi became a pilot and aviation manufacturers sponsored his keynotes on millennials to help reverse the shrinking pilot population.</p>
+			
+			<p>In 2015, the US Dept. of State began sponsoring his keynotes and cultural programs in Russia, Indonesia, Iraq, and Lebanon. He also founded Ravi Unites 
+			Schools—a large network of international K-12 schools whose classes participate in peer-to-peer real-time audio-video interactions hosted by Ravi. He 
+			believes that these opportunities create a path to world peace.</p>
+			
+			<p>While Ravi has been speaking at conferences, businesses, and educational institutions since 1999, it became his primary focus in 2015. Having already 
+			published a memoir (Simon & Schuster) and hundreds of magazine articles, the global pandemic of 2020 created the space for Ravi to complete his highly 
+			anticipated book, <em>PIVOT: Empowering Students Today to Succeed in an Unpredictable Tomorrow</em>.</p>
 		
 		`,
   },
+	{
+    image: `https://d3oaxt0bwkjnjn.cloudfront.net/images/chris-widener.jpg`,
+    name: `Chris Widener`,
+    company: `Motivational Speaker and Business Success Expert`,
+    title: `Best selling author of 21 books, world renown business expert top 50 motivational speakers in the WORLD`,
+    modelId: `tA2ey`,
+    description: `
+		
+			<p><strong>Chris Widener</strong> is a best-selling author, motivational speaker, and business success expert who has worked closely with Jim Rohn and Zig Ziglar. Chris 
+			hosted a popular television show with Zig, and co-authored books with Jim Rohn, a highly respected and recognized motivational speaker and educator.</p>
+			
+			<p>Chris is the author of 20 books which have been translated into 13 languages, including his book, The Art of Influence, which his popular keynote is based 
+			upon. He has been speaking professionally since 1988 and has given over 2,000 speeches to crowds as large as 25,000 people!</p>
+			
+			<p>He has also been named one of the <strong>Top 50 Speakers in the World, one of Inc. Magazine's Top 100 Leadership speakers, and is a member of the Motivational 
+			Speakers Hall of Fame.</strong> Chris’ 21st book will be released this Fall, <em>“Lasting Impact, Creating a Life and a Business that Lives Beyond You”.</em></p>
+		
+		`,
+  }
 ];
