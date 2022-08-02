7979915332476c0931e9821aefc71157ae8f8684
Derrick <derricktleem@gmail.com>
2022-08-02 00:04:03 +0000

    Add speakers

--- src/app/pages/2023/home.component.ts
+++ src/app/pages/2023/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 
+import { fsList } from "./shared/data";
+
 @Component({
   selector: "app-con-expo-2023",
   templateUrl: "./home.component.html",
   styles: [`
-    h1, h2, h3, h4, h5, p {
+    h1, h2, h3, h4, h5, li, p {
       color: #2D2926;
+    }
+    h1, h2, h3, h4, h5 {
+      font-weight: 500;
     }
     .largegreenbg {
       background-color: #0376cd;
@@ -14,6 +19,8 @@ import { Title } from "@angular/platform-browser";
   `],
 })
 export class ConExpo2023Component implements OnInit {
+
+  fss = fsList;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('IAHSP 2023 Educational Conference and EXPO');
---
+++ src/app/pages/2023/shared/data.ts
@@ -0,0 +1,22 @@
+export const fsList = [	
+  {
+    image: `https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/iahsp-con-expo-featured-speaker-jonny-fowler.png`,
+    fname: `Jonny`,
+    lname: `Fowler`
+  },
+	{
+    image: `https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/iahsp-con-expo-featured-speaker-Jennie-norris.png`,
+    fname: `Jennie`,
+    lname: `Norris`
+  },
+  {
+    image: `https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/iahsp-con-expo-featured-speaker-elaine-penhaul.png`,
+    fname: `Elaine`,
+    lname: `Penhaul`
+  },
+	{
+    image: `https://d3oaxt0bwkjnjn.cloudfront.net/iahspconexpo/images/iahsp-con-expo-featured-speaker-nikki-watson.png`,
+    fname: `Nikki`,
+    lname: `Watson`
+  }
+];
